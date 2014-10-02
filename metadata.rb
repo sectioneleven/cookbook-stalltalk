66e4adadced9226d9217580b45f4850f46fbd5c7
Lemuel Formacil <lemuelf@gmail.com>
2014-10-02 22:23:47 +0400

    `berks cookbook stalltalk`

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'stalltalk'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stalltalk'
+long_description 'Installs/Configures stalltalk'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: stalltalk
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
